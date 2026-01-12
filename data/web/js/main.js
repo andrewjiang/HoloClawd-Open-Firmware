1a061fbd937e1e5c8f84c8ca97e10d11e74fb6dc
TimesZ <crashzeus@protonmail.com>
2026-01-12 19:59:41 +0100

    feat(web): rework page header/footer load
    Make a header and footer common for all page
    Loaded with dom manipulation

--- data/web/js/main.js
+++ data/web/js/main.js
@@ -1,3 +1,38 @@
+function includeHTML(id, url, callback) {
+  fetch(url)
+    .then((response) => response.text())
+    .then((data) => {
+      document.getElementById(id).innerHTML = data;
+      if (typeof callback === "function") callback();
+    });
+}
+
+function setHeaderTitle(title) {
+  const interval = setInterval(() => {
+    const h1 = document.getElementById("header-title");
+    if (h1) {
+      h1.textContent = title;
+      clearInterval(interval);
+    }
+  }, 20);
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  if (document.getElementById("header-placeholder")) {
+    includeHTML("header-placeholder", "./header.html", () => {
+      let pageTitle =
+        document.title && document.title.trim()
+          ? document.title.trim()
+          : "Placeholder Title";
+      setHeaderTitle(pageTitle);
+    });
+  }
+  if (document.getElementById("footer-placeholder")) {
+    includeHTML("footer-placeholder", "./footer.html");
+  }
+});
+
+//
 function rebootHandler() {
   return {
     loading: false,
